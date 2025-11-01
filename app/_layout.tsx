7f76ef52a5ac3afcc68f63bd19203c926fe6c36a
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-11-01 09:47:42 -0500

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { AuthProvider } from '@/hooks/AuthContext';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -28,10 +29,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
+  
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
