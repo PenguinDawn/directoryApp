f15345f5981a9896e19b83d2d1e7868398b87e12
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2025-11-07 12:17:12 -0600

    auth update

--- hooks/AuthContext.tsx
+++ hooks/AuthContext.tsx
@@ -1,10 +1,23 @@
-import { appwrite } from "@/lib/appwrite";
-import { createContext, useContext, useEffect, useState } from "react";
+// import { appwrite } from "@/lib/appwrite";
+import { createAppWriteService, MemberRow } from "@/lib/appwrite";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { Models } from "react-native-appwrite";
+
+		// low-level objects
+		// client,
+		// account,
+		// tables,
+		// // high-level helpers
+		// registerWithEmail,
+		// loginWithEmail,
+		// getCurrentUser,
+		// logoutCurrentDevice,
+		// getMemberByUserId,
 
 type AuthContentType = {
     user: Models.User<Models.Preferences> | null;
     loading: boolean;
+    member: MemberRow | null;
     login: (email: string, password: string) => Promise<void>;
     register: (email: string, password: string, name: string) => Promise<void>;
     logout: () => Promise<void>;
@@ -13,14 +26,16 @@ type AuthContentType = {
 const AuthContext = createContext<AuthContentType | undefined>(undefined);
 export function AuthProvider({children}: {children:React.ReactNode}) {
     // our two states
+    const appwriteService = createAppWriteService(null);
     const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null);
     const [loading, setLoading] = useState(true)
+    const [member, setMember] = useState<MemberRow | null>(null)
 
     // getting the user if they're logged in
     useEffect( () => {
         (async () => {
         // get current user
-        const currentUser = await appwrite.getCurrentUser()
+        const currentUser = await appwriteService.getCurrentUser()
         setUser(currentUser)
         setLoading(false);
         }
@@ -29,28 +44,51 @@ export function AuthProvider({children}: {children:React.ReactNode}) {
 
     // logging in the user
     async function login(email: string, password: string) {
-        const loggedInUser = await appwrite.loginWithEmail({email, password})
+        const loggedInUser = await appwriteService.loginWithEmail({email, password})
         // updates the user
         setUser(loggedInUser);
     }
 
     async function register(email: string, password: string, name: string) {
-        const loggedInUser = await appwrite.registerWithEmail({email, password, name})
+        const loggedInUser = await appwriteService.registerWithEmail({email, password, name})
         // updates the user
         setUser(loggedInUser)
     }
 
      async function logout() {
-        await appwrite.logoutCurrentDevice();
+        await appwriteService.logoutCurrentDevice();
         setUser(null);
         setLoading(false);
     }
 
 
+    const loadUser = useCallback(async () => {
+        setLoading(true)
+        try {
+            const user = await appwriteService.getCurrentUser();
+            if(user) {
+                const mem = await appwriteService.getMemberByUserId(user.$id)
+                setMember(mem)
+            }
+            else {
+                setMember(null)
+            }
+        }
+        finally {
+            setLoading(false)
+        }
+    }, [appwriteService])
+
+
+
+    useEffect(() => {
+        loadUser()
+    }, [loadUser])
+
 
 
     return (
-        <AuthContext.Provider value={{user, loading, login, register, logout}}>
+        <AuthContext.Provider value={{user, member, loading, login, register, logout}}>
             {children}
         </AuthContext.Provider>
     )
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,47 +1,98 @@
-import { Account, Client, ID, Models } from "react-native-appwrite";
+import { Account, Client, ID, Models, Query, TablesDB } from "react-native-appwrite";
 import "react-native-url-polyfill/auto";
 
 const APPWRITE_ENDPOINT = "https://nyc.cloud.appwrite.io/v1"
 const APPWRITE_PROJECT_ID = "6908d1580030732fe1df";
 const APPWRITE_PLATFORM = "Andriod"
+const DATABASE_ID = "6908d1de0004883c9d8a";
+const MEMEBERS_ID = "membersofclub";
 
-const client = new Client()
-	.setEndpoint(APPWRITE_ENDPOINT)
-	.setPlatform(APPWRITE_PLATFORM)
+export interface MemberRow extends Models.Row{
+	firstName: string,
+	lastName: string,
+	userID: string,
+	phone: string,
+	email: string,
+	showEmail: boolean,
+	club?: string,
+	showPhone: boolean,
+	office?: string,
+	status: string,
+	classification: string,
+	imgsrc: string,
+}
 
-const account = new Account(client)
 
-async function registerWithEmail({email, password, name} : {email:string, password: string, name:string}) {
+export function createAppWriteService(config) {
+	const client = new Client()
+		.setEndpoint(APPWRITE_ENDPOINT)
+		.setPlatform(APPWRITE_PLATFORM)
+		.setProject(APPWRITE_PROJECT_ID)
+
+	const account = new Account(client);
+	const tables = new TablesDB(client);
+
+	// auth
+	async function registerWithEmail({email, password, name} : {email:string, password: string, name:string}) {
 	await account.create({userId: ID.unique(), email, password, name})
 	await account.createEmailPasswordSession({email, password})
-	return await account.get<Models.User<Models.Preferences>>()
-}
+	return await account.get<Models.User<Models.Preferences>>()}
 
-async function loginWithEmail({email, password} : {email:string, password: string}) {
+	async function loginWithEmail({email, password} : {email:string, password: string}) {
 	await account.createEmailPasswordSession({email, password})
 	return await account.get<Models.User<Models.Preferences>>()
 }
 
-async function getCurrentUser() {
-	try {
-		const user = await account.get<Models.User<Models.Preferences>>()
-		return user;
+	async function getCurrentUser() {
+		try {
+			const user = await account.get<Models.User<Models.Preferences>>()
+			return user;
+		}
+		catch {
+			return null;
+		}
 	}
-	catch {
-		return null;
-	}
-}
 
-async function logoutCurrentDevice() {
+	async function logoutCurrentDevice() {
 	await account.deleteSession({sessionId: "current"});
+	}
+
+	// members and database stuff
+	const getMemberByUserId = async (userID: string):Promise<MemberRow> => {
+		const response = await tables.listRows<MemberRow>({
+			databaseId: DATABASE_ID,
+			tableId: MEMEBERS_ID,
+			queries: [Query.equal('userID', userID), Query.limit(1)]
+		})
+		return response.rows[0] ?? null
+	}
+
+	return {
+		// low-level objects
+		client,
+		account,
+		tables,
+		// high-level helpers
+		registerWithEmail,
+		loginWithEmail,
+		getCurrentUser,
+		logoutCurrentDevice,
+		getMemberByUserId,
+	}
+
+
 }
 
-// our object
-export const appwrite = {
-    client,
-    account,
-    registerWithEmail,
-    loginWithEmail,
-	getCurrentUser,
-	logoutCurrentDevice,
-}
+
+
+
+
+// // our object
+// export const appwrite = {
+//     client,
+//     account,
+//     registerWithEmail,
+//     loginWithEmail,
+// 	getCurrentUser,
+// 	logoutCurrentDevice,
+// }
